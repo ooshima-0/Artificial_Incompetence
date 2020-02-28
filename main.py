658d283353b18b9288a8ff955e4c38d36ad02d2d
ooshima-0 <oick5426@yahoo.co.jp>
2020-02-29 00:41:50 +0900

    Add Unmo class

--- main.py
+++ main.py
@@ -17,3 +17,32 @@ class Responder:
 	def name(self):
 		"""応答オブジェクトをの名前"""
 		return self._name
+
+class Unmo:
+	"""人工無脳コアの名前
+	
+	プロパティ：
+	name -- 人工無脳コアの名前
+	responder_name -- 現在の応答クラスの名前
+	"""
+
+	def __init__(self, name):
+		"""文字列を受け取り、コアインスタンスの名前に設定する
+		'What' Responderインスタンスを作成し保持する
+		"""
+		self._name = name
+		self._responder = Responder("What")
+	
+	def dialogue(self, text):
+		"""ユーザー方の入力を受け取り、Responderに処理させた結果を返す"""
+		return self._responder.response(text)
+	
+	@property
+	def name(self):
+		"""人工無脳インスタンスの名前"""
+		return self._name
+
+	@property
+	def responder_name(self):
+		"""保持しているResponderの名前"""
+		return self._responder.name
